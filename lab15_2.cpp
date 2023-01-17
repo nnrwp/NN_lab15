7948f08bf35a551f3245d3b7485fda2a7efb34a6
nnrwp <rawipa_s@cmu.ac.th>
2023-01-17 17:01:22 +0700

    completed

--- lab15_2.cpp
+++ lab15_2.cpp
@@ -19,3 +19,42 @@ int main(){
 	showMatrix(B);
 	return 0;
 }
+
+void inputMatrix(double ip[][N]){
+	for(int i = 0; i < N; i++){
+		cout << "Row " << i+1 << ": ";
+		for(int j = 0; j < N; j++){
+			cin >> ip[i][j];
+		}
+	}
+}
+
+void findLocalMax(const double a[][N], bool b[][N]){
+	for(int i = 0; i < N; i++){
+		for(int j = 0; j < N; j++){
+			b[i][j] = 0;
+		}
+	}
+	for(int i = 1; i < N-1; i++){
+		for(int j = 1;j < N-1; j++){
+			if(a[i][j] >= a[i-1][j] && a[i][j] >= a[i+1][j] && a[i][j] >= a[i][j-1] && a[i][j] >= a[i][j+1]){
+				b[i][j] = 1;
+			}else{
+				b[i][j] = 0;
+			}
+		}
+	}
+}
+
+void showMatrix(const bool s[][N]){
+	for(int i = 0; i < N; i++){
+		for(int j = 0; j < N; j++){
+			if(s[i][j] == 1){
+				cout << "1 ";
+			}else{
+				cout << "0 ";
+			}
+		}
+		cout << endl;
+	}
+}
