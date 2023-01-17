e16648a71563477c35a4ab17e696caf2742c6803
nnrwp <rawipa_s@cmu.ac.th>
2023-01-17 15:51:13 +0700

    completed

--- lab15_1.cpp
+++ lab15_1.cpp
@@ -3,7 +3,24 @@ using namespace std;
 
 template <typename T>
 void insertionSort(T d[],int N){
+	for(int i = 1; i < N; i++){
+		for(int j = i; j > 0; j--){
+			T x = d[j];
+			if(d[j] > d[j-1]){
+				d[j] = d[j-1];
+				d[j-1] = x;
+			}else{
+				continue;
+			}
+		}
+		cout << "Pass " << i << ":";
+		for(int k = 0; k < N; k++){
+			cout << d[k] << " ";
+		}
+		cout << "\n";
 
+	}
+	
 }
 
 int main(){
